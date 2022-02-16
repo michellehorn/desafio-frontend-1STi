6d68d3c6385b863e5b9e97b7b31c06bcf7e917e5
Michelle Horn <michellehorn@live.com>
2022-02-16 09:16:06 -0300

    rename card

--- src/components/Card/index.js
+++
@@ -1,7 +0,0 @@
-import Card from "./Card";
-import MainInfo from "./MainInfo";
-import Overline from "./Overline";
-
-Card.Overline = Overline;
-Card.MainInfo = MainInfo;
-export { Card as default };
---
+++ src/components/WeatherCard/index.js
@@ -0,0 +1,7 @@
+import WeatherCard from "./WeatherCard";
+import MainInfo from "./MainInfo";
+import Overline from "./Overline";
+
+WeatherCard.Overline = Overline;
+WeatherCard.MainInfo = MainInfo;
+export { WeatherCard as default };
